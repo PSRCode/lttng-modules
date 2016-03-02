fc399902f4e9fce55ee347f4e9f7422c10e59a0d
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2016-03-02 12:29:55 -0500

    Fix: system call instrumentation overrides
    Add missing OVERRIDE_32_clone and OVERRIDE_64_clone to enable the
    "clone" system call override.
    
    Add missing mmap2 system call instrumentation override for x86-32.
    
    On arm32 and powerpc32, move the mmap2 system call instrumentation from
    "pointers" to "integers" override header, and add the missing return
    value (sc_exit).
    
    Add missing output return values to arm_fadvise64_64, sync_file_range2
    on arm32.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- instrumentation/syscalls/headers/powerpc-32-syscalls-3.0.34_integers_override.h
+++ instrumentation/syscalls/headers/powerpc-32-syscalls-3.0.34_integers_override.h
@@ -1,9 +1,43 @@
 #ifndef CREATE_SYSCALL_TABLE
 
+#define OVERRIDE_32_mmap2
+SC_LTTNG_TRACEPOINT_EVENT(mmap2,
+	TP_PROTO(sc_exit(unsigned long ret,)
+		unsigned long addr, unsigned long len,
+		unsigned long prot, unsigned long flags,
+		unsigned long fd, unsigned long pgoff),
+	TP_ARGS(sc_exit(ret,) addr, len, prot, flags, fd, pgoff),
+	TP_FIELDS(sc_exit(ctf_integer_hex(unsigned long, ret, ret))
+		sc_in(ctf_integer_hex(unsigned long, addr, addr))
+		sc_in(ctf_integer(size_t, len, len))
+		sc_in(ctf_integer(int, prot, prot))
+		sc_in(ctf_integer(int, flags, flags))
+		sc_in(ctf_integer(int, fd, fd))
+		sc_in(ctf_integer(off_t, pgoff, pgoff))
+	)
+)
+
+#define OVERRIDE_32_sync_file_range2
+SC_LTTNG_TRACEPOINT_EVENT(sync_file_range2,
+	TP_PROTO(sc_exit(long ret,) int fd, unsigned int flags,
+		loff_t offset, loff_t nbytes),
+	TP_ARGS(sc_exit(ret,) fd, flags, offset, nbytes),
+	TP_FIELDS(sc_exit(ctf_integer(long, ret, ret))
+		sc_in(ctf_integer(int, fd, fd))
+		sc_in(ctf_integer(unsigned int, flags, flags))
+		sc_in(ctf_integer(loff_t, offset, offset))
+		sc_in(ctf_integer(loff_t, nbytes, nbytes))
+	)
+)
+
 #else	/* CREATE_SYSCALL_TABLE */
 
 #define OVERRIDE_TABLE_32_mmap
 TRACE_SYSCALL_TABLE(mmap, mmap, 90, 6)
+#define OVERRIDE_TABLE_32_mmap2
+TRACE_SYSCALL_TABLE(mmap2, mmap2, 192, 6)
+#define OVERRIDE_TABLE_32_sync_file_range2
+TRACE_SYSCALL_TABLE(sync_file_range2, sync_file_range2, 308, 4)
 
 #endif /* CREATE_SYSCALL_TABLE */
 
--- instrumentation/syscalls/headers/x86-32-syscalls-3.1.0-rc6_integers_override.h
+++ instrumentation/syscalls/headers/x86-32-syscalls-3.1.0-rc6_integers_override.h
@@ -30,9 +30,35 @@
 
 #endif
 
-#ifdef CREATE_SYSCALL_TABLE
+#ifndef CREATE_SYSCALL_TABLE
+
+/* mmap_pgoff is kernel internal. mmap2 is exposed in unistd.h. */
+#define OVERRIDE_32_mmap_pgoff
+#define OVERRIDE_32_mmap2
+SC_LTTNG_TRACEPOINT_EVENT(mmap2,
+	TP_PROTO(sc_exit(unsigned long ret,)
+		unsigned long addr, unsigned long len,
+		unsigned long prot, unsigned long flags,
+		unsigned long fd, unsigned long pgoff),
+	TP_ARGS(sc_exit(ret,) addr, len, prot, flags, fd, pgoff),
+	TP_FIELDS(sc_exit(ctf_integer_hex(unsigned long, ret, ret))
+		sc_in(ctf_integer_hex(unsigned long, addr, addr))
+		sc_in(ctf_integer(size_t, len, len))
+		sc_in(ctf_integer(int, prot, prot))
+		sc_in(ctf_integer(int, flags, flags))
+		sc_in(ctf_integer(int, fd, fd))
+		sc_in(ctf_integer(off_t, pgoff, pgoff))
+	)
+)
+
+#else /* #ifndef CREATE_SYSCALL_TABLE */
 
 #define OVERRIDE_TABLE_32_mmap
 TRACE_SYSCALL_TABLE(mmap, mmap, 90, 6)
 
-#endif
+/* mmap_pgoff is kernel internal. mmap2 is exposed in unistd.h. */
+#define OVERRIDE_TABLE_32_mmap_pgoff
+#define OVERRIDE_TABLE_32_mmap2
+TRACE_SYSCALL_TABLE(mmap2, mmap2, 192, 6)
+
+#endif /* #else #ifndef CREATE_SYSCALL_TABLE */
